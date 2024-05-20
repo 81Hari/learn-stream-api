d8f9790b185702acada52cf658cf0afb90ad085a
Srihari M <sri.hari@zohocorp.com>
2024-05-20 13:22:40 +0530

    feat: lambda walkthrough

---
+++ lambda-examples/src/main/java/LambdaExample.java
@@ -0,0 +1,40 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Comparator;
+
+/**
+ * Hello world!
+ *
+ */
+public class LambdaExample
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(LambdaExample.class);
+    public static void main( String[] args ) throws InterruptedException {
+        Runnable legacyRunnable = new Runnable() {
+            @Override
+            public void run() {
+                LOGGER.info("Legacy Runnable");
+            }
+        };
+
+        Runnable lambdaRunnable = ()->LOGGER.info("Lambda Runnable");
+        Thread thread1, thread2;
+        thread1 = new Thread(legacyRunnable);
+        thread2 = new Thread(lambdaRunnable);
+        thread1.start();
+        thread2.start();
+
+        Comparator<Integer> legacyComparator = new Comparator<Integer>() {
+                    @Override
+                    public int compare(Integer o1, Integer o2) {
+                        return o1.compareTo(o2);
+                    }
+                };
+        LOGGER.info("Legacy Comparator: {}" , legacyComparator.compare(2,3));
+
+        Comparator<Integer> lambdaComparator = (o1,o2)->o1.compareTo(o2);
+        LOGGER.info("Lambda Comparator: {}" , lambdaComparator.compare(3,2));
+
+    }
+}
